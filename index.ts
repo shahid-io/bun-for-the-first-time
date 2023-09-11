127af665563b2e65410bdd53bc42d0662ca52d1d
shahid <razashahid532@gmail.com>
2023-09-11 15:09:41 +0530

    Bun for the first time

---
+++ index.ts
@@ -0,0 +1,10 @@
+import figlet from "figlet";
+
+const server = Bun.serve({
+    fetch() {
+        const body = figlet.textSync('Bun!');
+        return new Response(body);
+    },
+    port: 3000,
+});
+console.log(`Listening on http://localhost:${server.port} ...`);
